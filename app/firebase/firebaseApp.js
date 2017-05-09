4bdebbebbf777eb9aad0dada4f7fef5b55c865d0
lubaochuan <lubaochuan@gmail.com>
2017-05-09 09:59:27 -0500

    ensure firebase.initializeApp is called exactly once

--- app/firebase/firebaseApp.js
+++ app/firebase/firebaseApp.js
@@ -1,15 +1,18 @@
-import { initializeApp } from 'firebase'
+import * as firebase from 'firebase'
 import config from './config'
 import { itemActionCreators } from '../actions/items'
 import { userActionCreators } from '../actions/user'
 
 export default class Firebase {
   static initialize(callback) {
-    initializeApp({
-      apiKey: config.API_KEY,
-      authDomain: config.AUTH_DOMAIN,
-      databaseURL: config.DATABASE_URL,
-      storageBucket: config.STORAGE_BUCKET
-    }).auth().onAuthStateChanged(callback);
+    if (firebase.apps.length === 0) {
+      firebase.initializeApp({
+        apiKey: config.API_KEY,
+        authDomain: config.AUTH_DOMAIN,
+        databaseURL: config.DATABASE_URL,
+        storageBucket: config.STORAGE_BUCKET
+      }).auth().onAuthStateChanged(callback);
+    }
+    return firebase
   }
 }
